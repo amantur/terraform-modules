dc36ef370e1dee495b505af3163a0626fc8b999c
aman.tur <aman.tur@gmail.com>
2022-08-18 00:27:03 +1000

    Added few variable for cluster default node pool. Added validations and formatted files

--- azurerm/azurerm_kubernetes_cluster/variables.tf
+++ azurerm/azurerm_kubernetes_cluster/variables.tf
@@ -23,11 +23,6 @@ variable "dns_prefix" {
   description = "DNS Prefix"
 }
 
-variable "node_count" {
-  type        = string
-  description = "Node Count"
-}
-
 variable "node_size" {
   type        = string
   description = "Node Size"
@@ -36,6 +31,43 @@ variable "node_size" {
 variable "node_disk_size" {
   type        = string
   description = "Node Disk Size"
+}
+
+variable "enable_autoscaling" {
+  type = bool
+}
+
+variable "node_count" {
+  type        = number
+  description = "Number of node that should exist in cluster. It is required if 'enable_auto_scaling' is set to false."
+
+  validation {
+    condition     = var.node_count >= 1 && var.node_count <= 1000
+    error_message = "The node count value should be between 1 and 1000. Also, between min and max node counts. It is required if 'enable_auto_scaling' is set to false."
+  }
+}
+
+variable "min_node_count" {
+  type        = number
+  description = "Minimum number of nodes in cluster. Should be between 1 and 1000"
+  validation {
+    condition     = (var.min_node_count >= 1 && var.min_node_count <= 1000)
+    error_message = "The Min Node Count should be between 1 and 1000."
+  }
+}
+
+variable "max_node_count" {
+  type        = number
+  description = "Maximum number of nodes in cluster. Should be between 1 and 1000"
+  validation {
+    condition     = (var.max_node_count >= 1 && var.max_node_count <= 1000)
+    error_message = "The Max Node Count should be between 1 and 1000."
+  }
+}
+
+variable "max_pod_count" {
+  type        = number
+  description = "The maximum number of pods that can run on each agent."
 }
 
 variable "kubernetes_network_plugin" {
