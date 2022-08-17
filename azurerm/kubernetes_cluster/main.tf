7ddae19c266de5d2a3ab2e7aa4342117c8cc3c7b
aman.tur <aman.tur@gmail.com>
2022-08-18 00:27:03 +1000

    Added few variable for cluster default node pool. Added validations and formatted files

--- azurerm/kubernetes_cluster/main.tf
+++ azurerm/kubernetes_cluster/main.tf
@@ -1,21 +1,24 @@
 resource "azurerm_kubernetes_cluster" "default" {
-  name                    = var.name
-  dns_prefix              = var.dns_prefix
-  location                = var.location
-  resource_group_name     = var.resource_group_name
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  dns_prefix          = var.dns_prefix
+
   private_cluster_enabled = var.private_cluster_enabled
   //api_server_authorized_ip_ranges = ["34.83.219.216/32"]
+
   node_resource_group = "${var.resource_group_name}-node"
+
   default_node_pool {
     name                = "agentpool"
-    node_count          = var.node_count
     vm_size             = var.node_size
     os_disk_size_gb     = var.node_disk_size
     vnet_subnet_id      = var.kubernetes_subnet_id
-    enable_auto_scaling = true
-    min_count           = 1
-    max_count           = 10
-    max_pods            = 200
+    enable_auto_scaling = var.enable_autoscaling
+    node_count          = var.node_count
+    min_count           = var.min_node_count
+    max_count           = var.max_node_count
+    max_pods            = var.max_pod_count
   }
 
   identity {
--- azurerm/kubernetes_cluster_node_pool/main.tf
+++ azurerm/kubernetes_cluster_node_pool/main.tf
@@ -3,9 +3,9 @@ resource "azurerm_kubernetes_cluster_node_pool" "default" {
   kubernetes_cluster_id = var.kubernetes_cluster_id
   vm_size               = var.vm_size
   enable_auto_scaling   = true
-  max_count             = var.max_count
-  min_count             = var.min_count
-  max_pods              = 250
+  max_count             = var.max_node_count
+  min_count             = var.min_node_count
+  max_pods              = var.max_pod_count
   vnet_subnet_id        = var.kubernetes_subnet_id
 
   tags = var.tags
